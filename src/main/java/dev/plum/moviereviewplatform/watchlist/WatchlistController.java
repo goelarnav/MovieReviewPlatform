55ee96a19b3922cbbc5ca2dfafbdf288832920fb
goelarnav <132059026+goelarnav@users.noreply.github.com>
2024-08-16 16:34:48 +0530

    Update WatchlistController.java

--- src/main/java/dev/plum/moviereviewplatform/watchlist/WatchlistController.java
+++ src/main/java/dev/plum/moviereviewplatform/watchlist/WatchlistController.java
@@ -28,15 +28,19 @@ public class WatchlistController {
 
     @GetMapping("/{login}")
     public ResponseEntity<List<Movie>> findWatchlist(@PathVariable String login){
+        //find user id associated with given login
         ObjectId userId = new ObjectId(userService.findUserId(login));
-        Optional<Watchlist> foundWatchlist = watchlistService.getWatchlist(userId);
+        //retrieve watchlist associated with given object id
+        Optional<Watchlist> foundWatchlist = watchlistService.getWatchlist(userId); //optional if it doesnt exist
         if(foundWatchlist.isPresent()){
             List<Movie> watchlistMovies = new ArrayList<>();
+            //iterates over movieId's in found watchlist and add movie object corresponding to the id's
             for(ObjectId movieId : foundWatchlist.get().getMovieList()){
                 watchlistMovies.add(movieService.findMovieById(movieId));
             }
             return new ResponseEntity<List<Movie>>(watchlistMovies, HttpStatus.OK);
         } else{
+            //an empty watchlist is created
             Watchlist savedWatchlist = watchlistService.saveNewWatchList(userId);
             List<Movie> emptyList = new ArrayList<Movie>();
             return new ResponseEntity<List<Movie>>(emptyList, HttpStatus.OK);
@@ -44,6 +48,7 @@ public class WatchlistController {
     }
 
     @PostMapping
+    //retrieve body of the new movie being added
     public ResponseEntity<?> addMovieToWatchList(@RequestBody AddMovieToWatchlistRequest addMovieToWatchlistRequest, UriComponentsBuilder ucb){
         Integer imdbId = addMovieToWatchlistRequest.getImdbid();
         ObjectId movieId = movieService.findObjectIdByImdbId(imdbId);
